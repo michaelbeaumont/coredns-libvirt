067e613fac6d10e358746b7f63bb8215d268a914
Mike Beaumont <mjboamail@gmail.com>
2023-11-23 04:15:31 +0100

    feat: support trimming suffix

--- README.md
+++ README.md
@@ -5,13 +5,18 @@ similar to the `nss` plugin `libvirt_guest`.
 
 ## Usage
 
-Currently this plugin can only be used in the following mode.
+Currently this plugin can only be used in the `guest` mode.
 
 ### Guest name
 
 The functionality of `libvirt guest` is analogous to the `libvirt_guest` `nss`
 plugin, where we look for a match on the name of the libvirt domain, not
 necessarily a hostname.
+
+### Zones
+
+If your zone isn't root `.`, you'll likely want to include the `trim_suffix`
+directive so you search for the correct name in your guests.
 
 ### Filtering by network
 
@@ -21,9 +26,9 @@ them with the `keep` directive.
 ## Example
 
 ```
-vm.:1053 {
-  rewrite name suffix .vm .
+vm:1053 {
   libvirt guest {
+    trim_suffix vm
     keep 10.101.0.0/24
   }
 }
--- plugin.go
+++ plugin.go
@@ -28,33 +28,40 @@ func setup(c *caddy.Controller) error {
 		return plugin.Error(pluginName, fmt.Errorf("expected 'guest' as argument"))
 	}
 
+	var trimSuffix string
 	var rules []subnetRules
 
 	for c.NextBlock() {
 		var kind ruleKind
 		switch c.Val() {
+		case "trim_suffix":
+			if !c.NextArg() {
+				return plugin.Error(pluginName, c.ArgErr())
+			}
+			trimSuffix = c.Val()
 		case "keep":
 			kind = keep
+			if !c.NextArg() {
+				return plugin.Error(pluginName, c.ArgErr())
+			}
+			cidr := c.Val()
+			_, net, err := net.ParseCIDR(cidr)
+			if err != nil {
+				return plugin.Error(pluginName, err)
+			}
+			rules = append(rules, subnetRules{
+				kind: kind,
+				cidr: *net,
+			})
 		default:
 			return plugin.Error(pluginName, fmt.Errorf("unexpected argument: %s", c.Val()))
 		}
-		if !c.NextArg() {
-			return plugin.Error(pluginName, c.ArgErr())
-		}
-		cidr := c.Val()
-		_, net, err := net.ParseCIDR(cidr)
-		if err != nil {
-			return plugin.Error(pluginName, err)
-		}
-		rules = append(rules, subnetRules{
-			kind: kind,
-			cidr: *net,
-		})
 		if len(c.RemainingArgs()) > 0 {
 			return plugin.Error(pluginName, fmt.Errorf("unexpected arguments on line %d", c.Line()))
 		}
 	}
 
+	h.trimSuffix = trimSuffix
 	h.rules = rules
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
